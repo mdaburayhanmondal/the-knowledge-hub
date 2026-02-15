17f3de4acbb738a826fc64176c50334148ecdddd
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-02-15 20:32:03 +0600

    middleware for token verification

---
+++ server/middleware/verifyToken.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: 'Unauthorized! No token provided.' });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(403)
+        .json({ message: 'Forbidden! Invalid or expired token.' });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+module.exports = verifyToken;
