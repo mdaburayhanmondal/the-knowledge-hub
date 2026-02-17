53606a2ff3bd20bffeddb08864cfe0aeada49e86
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-02-17 20:39:30 +0600

    fixed front-end url

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://theknowledgehub.netlfiy.app'],
+    origin: ['http://localhost:5173', 'https://theknowledgehub.netlify.app'],
     credentials: true,
   }),
 );
