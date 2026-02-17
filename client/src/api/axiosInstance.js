b24c16ac8ae0c42906d0c5d7a99f1a230f2bfa5b
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-02-17 21:47:35 +0600

    fixed API url in .env and added alt local url

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // DRY Base URL
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
+  baseURL: import.meta.env.VITE_API_URL || import.meta.env.VITE_BASE_URL,
   withCredentials: true,
 });
 
